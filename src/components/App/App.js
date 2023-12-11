33fef59101571699d146b6abfcafb284c2a55b8e
unknown <giokhabu@gamil.com>
2023-12-11 14:11:17 +0400

    feat: correct some styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,25 +14,27 @@ function App() {
   return (
     <div className="App">
       <main className="App-header">
-        <h1>ADVICE # {data && data?.slip.id}</h1>
-        <p>{`“${data && data?.slip.advice}”`}</p>
-        <div className="line-button-wrapper">
-          <div className="lines-wrapper">
-            <div className="horizontal"></div>
-            <div className="vertical left"></div>
-            <div className="vertical right"></div>
-            <div className="horizontal"></div>
+        <div className="text-wrapper">
+          <h1>ADVICE # {data && data?.slip.id}</h1>
+          <p>{`“${data && data?.slip.advice}”`}</p>
+          <div className="line-button-wrapper">
+            <div className="lines-wrapper">
+              <div className="horizontal"></div>
+              <div className="vertical left"></div>
+              <div className="vertical right"></div>
+              <div className="horizontal"></div>
+            </div>
           </div>
-          <button
-            onClick={() => {
-              fetch('https://api.adviceslip.com/advice')
-                .then((response) => response.json())
-                .then((data) => setData(data));
-            }}
-          >
-            <img src={dice} alt="dice" />
-          </button>
         </div>
+        <button
+          onClick={() => {
+            fetch('https://api.adviceslip.com/advice')
+              .then((response) => response.json())
+              .then((data) => setData(data));
+          }}
+        >
+          <img src={dice} alt="dice" />
+        </button>
       </main>
     </div>
   );
